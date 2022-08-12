7086f0a071c198727fc1d70fdf72eef1783c8808
muchisx <mnm.soul@gmail.com>
2022-08-12 16:40:51 -0500

    added console log easter egg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Template from '@templates/Template.js';
 import '@styles/main.css'
 import '@styles/vars.styl'
 
-console.log('hola');
+console.log('hey sexy curious dev');
 
 (async function App() {
   const main = null || document.getElementById('main');
